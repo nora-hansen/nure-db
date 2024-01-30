ab84ba55e5f228a04d86771114ccda6abf8d427a
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-30 15:03:45 +0100

    Basic getAll function

--- src/main/java/eu/nurenur/api/controllers/ImageController.java
+++ src/main/java/eu/nurenur/api/controllers/ImageController.java
@@ -1,7 +1,15 @@
 package eu.nurenur.api.controllers;
 
+import eu.nurenur.api.models.Image;
+import eu.nurenur.api.repositories.ImageRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("images")
@@ -11,4 +19,18 @@ public class ImageController {
     public ImageController()    {
         this.imageRepo = new ImageRepository();
     }
+
+    @GetMapping
+    public ArrayList<Image> getAll()    {
+        return this.imageRepo.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Image getOne(@PathVariable int id)   {
+        if(this.imageRepo.findOne(id) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");
+        return this.imageRepo.findOne(id);
+    }
+
+
 }
--- src/main/java/eu/nurenur/api/models/Image.java
+++ src/main/java/eu/nurenur/api/models/Image.java
@@ -3,19 +3,58 @@ package eu.nurenur.api.models;
 import java.util.ArrayList;
 
 public class Image {
-    int id;
-    String title;
-    String description;
-    ArrayList<String> tags;
-    String date;
-    // ArrayList<Character> characters;
+    // REIMPLEMENT BETTER!!
+    private static int nextID;
+    private int id;
+    private String title;
+    private String description;
+    private ArrayList<String> tags;
+    private String date;
 
     public Image() {}
 
     public Image(String title, String description, ArrayList<String> tags, String date)  {
+        this.id = nextID;
+        nextID++;
         this.title = title;
         this.description = description;
         this.tags = tags;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
         this.date = date;
     }
 }
